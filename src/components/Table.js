e6dc8eaef70b8d16617fa9a84b0dcf3a2ede260c
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-11-03 11:47:45 +0530

    user

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,47 +3,99 @@ import axios from "axios";
 
 const Table = () => {
 
-    const [tableData, setTableData] = useState(null);
+    // const [tableData, setTableData] = useState(null);
+
+    // function fetchData() {
+    //     axios.get("https://reqres.in/api/users").then((response) => {
+
+    //         setTableData(response.data.data);
+    //     });
+    // }
+
+    // return (
+    //     <div className="Users">
+    //         <div className="upper">
+    //             <h2>Blue Whales</h2>
+    //             <button onClick={fetchData} id="fetch-btn">Get User List</button>
+    //         </div>
+
+    //         <table>
+    //             <thead className="thead">
+    //                 <tr>
+    //                     <th>First Name</th>
+    //                     <th>Last Name</th>
+    //                     <th>Email</th>
+    //                     <th>Avatar</th>
+    //                 </tr>
+    //             </thead>
+
+    //             {
+    //                 tableData === null ? <th>No data found to display</th> : <tbody>
+    //                     {
+    //                         tableData.map((items) => (
+    //                             <tr>
+    //                                 <td>{items.first_name}</td>
+    //                                 <td>{items.last_name}</td>
+    //                                 <td>{items.email}</td>
+    //                                 <td><img src={items.avatar} alt="logo" /></td>
+    //                             </tr>
+    //                         ))
+    //                     }
+    //                 </tbody>
+    //             }
+    //         </table>
+    //     </div>
+    const url = "https://reqres.in/api/users";
+    const [data, setData] = useState("");
 
     function fetchData() {
-        axios.get("https://reqres.in/api/users").then((response) => {
-
-            setTableData(response.data.data);
+        axios.get(url).then((response) => {
+            //   console.log(response.data.data);
+            setData(response.data.data);
         });
+
     }
 
     return (
-        <div className="Users">
-            <div className="upper">
-                <h2>Blue Whales</h2>
-                <button onClick={fetchData} id="fetch-btn">Get User List</button>
+        <div className='Users'>
+            <div className='Navbar'>
+                <h1>Blue Whales</h1>
+                <button className='btn' onClick={() => (fetchData())}>Get User list</button>
             </div>
+            <div className='Table'>
 
-            <table>
-                <thead className="thead">
-                    <tr>
+                <table >
+                    <thead>
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Email</th>
                         <th>Avatar</th>
-                    </tr>
-                </thead>
+                    </thead>
+                    <tbody>
 
-                {
-                    tableData === null ? <th>No data found to display</th> : <tbody>
                         {
-                            tableData.map((items) => (
-                                <tr>
-                                    <td>{items.first_name}</td>
-                                    <td>{items.last_name}</td>
-                                    <td>{items.email}</td>
-                                    <td><img src={items.avatar} alt="logo" /></td>
-                                </tr>
-                            ))
+                            !data ? <th>No data found to display.</th> :
+
+                                data.map((val) => (
+                                    <tr key={val.id}>
+                                        <td >
+                                            {val.first_name}
+                                        </td>
+                                        <td>
+                                            {val.last_name}
+                                        </td>
+                                        <td>
+                                            {val.email}
+                                        </td>
+                                        <td>
+                                            <img src={val.avatar} />
+                                        </td>
+                                    </tr>
+                                ))
                         }
                     </tbody>
-                }
-            </table>
+                </table>
+            </div>
         </div>
     )
 }
